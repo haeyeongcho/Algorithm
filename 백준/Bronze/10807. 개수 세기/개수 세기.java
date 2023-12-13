f6c3cf4079d94b6a0561214ae63189a0315afe8b
socilt <98633822+socilt@users.noreply.github.com>
2023-12-13 10:52:19 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int arr[] = new int[num];
+
+        for(int i=0;i<num;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = sc.nextInt();
+        int result = 0;
+
+        for(int i : arr){
+            if(i==ans)
+                result ++;
+        }
+
+        System.out.println(result);
+    }
+}
