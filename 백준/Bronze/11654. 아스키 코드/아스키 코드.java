bf56ffab3a4f49eba1aff80d27f3881d8263decb
socilt <98633822+socilt@users.noreply.github.com>
2023-12-28 07:51:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char s1 = sc.next().charAt(0);
+        System.out.println((int)s1);
+    }
+}
