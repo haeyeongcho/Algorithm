89265bc2a0603bd9ffd750d1ed73d2b9e3a5e725
socilt <98633822+socilt@users.noreply.github.com>
2023-12-30 06:43:43 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[9];
+        int max = 0;
+        int index = 0;
+
+        for(int i=0;i<nums.length;i++){
+            nums[i] = sc.nextInt();
+            if(nums[i] > max){
+                max = nums[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+
+    }
+}
