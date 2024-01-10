7c5170aa42f95c21257d891a0c6508ca91c1fd53
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-01-10 16:28:05 +0900

    [Bronze V] Title: A+B - 5, Time: 224 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
-            try {
-                int n1 = sc.nextInt();
-                int n2 = sc.nextInt();
-                if(n1 == 0 && n2 ==0){
-                    break;
-                }
-                System.out.println(n1 + n2);
-            } catch (Exception e) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0)
                 break;
-            }
+            System.out.println(a + b);
         }
     }
 }
