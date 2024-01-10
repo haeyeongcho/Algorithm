eb249ef03d1a92ed15abcfb8b984e8edbe555b30
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-01-10 09:42:05 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -9,35 +9,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(s);
         int num1 = Integer.parseInt(st.nextToken());
         int num2 = Integer.parseInt(st.nextToken());
-        int num1_ = num1;
-        int num2_ = num2;
 
         int gcd = 1;
-        int n = 0;
-        for (int j = 0; j <num2; j++) {
-            if (num1 > num2)
-                n = num2;
-            else
-                n = num1;
-            for (int i = 1; i <= n; i++) {
-                if (num1 % i == 0 && num2 % i == 0) {
-                    num1 /= i;
-                    num2 /= i;
-                    gcd *= i;
-                }
+        int min = Math.min(num1, num2);
+        for (int i = 2; i <= min; i++) {
+            while(num1 % i == 0 && num2 % i == 0) {
+                gcd *= i;
+                num1 /= i;
+                num2 /= i;
             }
         }
-        int lcm = 1;
-        int count1 = num1_;
-        int count2 = num2_;
-        while(num1_ != num2_){
-            if (num1_ > num2_) {
-                num2_ += count2;
-            } else if (num1_ < num2_) {
-                num1_+= count1;
-            }
-        }
-        lcm = num1_;
+        int lcm = num1 * num2 * gcd;
+
         bw.write(Integer.toString(gcd));
         bw.newLine();
         bw.write(Integer.toString(lcm));
