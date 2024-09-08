4f3190d4673e3b7822a8bfdba0522284754a4bda
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-09-08 15:42:34 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.15 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] charArray = my_string.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        String answer = new String(charArray);
+        return answer;
+    }
+}
