72e61d55a8e2c883ec935c7e7cc54d27de3632cb
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-09-20 22:08:25 +0900

    [level 0] Title: 모스부호 (1), Time: 3.03 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,92 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morses = letter.split(" ");
+        
+        for(String morse : morses){
+            switch(morse){
+                case ".-":
+                    answer += "a";
+                    break;
+                case "-...":
+                    answer += "b";
+                    break;
+                case "-.-.":
+                    answer += "c";
+                    break;
+                case "-..":
+                    answer += "d";
+                    break;
+                case ".":
+                    answer += "e";
+                    break;
+                case "..-.":
+                    answer += "f";
+                    break;
+                case "--.":
+                    answer += "g";
+                    break;
+                case "....":
+                    answer += "h";
+                    break;
+                case "..":
+                    answer += "i";
+                    break;
+                case ".---":
+                    answer += "j";
+                    break;
+                case "-.-":
+                    answer += "k";
+                    break;
+                case ".-..":
+                    answer += "l";
+                    break;
+                case "--":
+                    answer += "m";
+                    break;
+                case "-.":
+                    answer += "n";
+                    break;
+                case "---":
+                    answer += "o";
+                    break;
+                case ".--.":
+                    answer += "p";
+                    break;
+                case "--.-":
+                    answer += "q";
+                    break;
+                case ".-.":
+                    answer += "r";
+                    break;
+                case "...":
+                    answer += "s";
+                    break;
+                case "-":
+                    answer += "t";
+                    break;
+                case "..-":
+                    answer += "u";
+                    break;
+                case "...-":
+                    answer += "v";
+                    break;
+                case ".--":
+                    answer += "w";
+                    break;
+                case "-..-":
+                    answer += "x";
+                    break;
+                case "-.--":
+                    answer += "y";
+                    break;
+                case "--..":
+                    answer += "z";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return answer;
+    }
+}
