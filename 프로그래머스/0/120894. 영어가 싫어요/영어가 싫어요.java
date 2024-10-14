131a3ab4044cc9f83c2017b3c391084b71401703
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-10-15 06:38:42 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long solution(String numbers) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < numbers.length(); i++){
+            if(numbers.charAt(i) == 'z'){
+                sb.append("0");
+                i+=3;
+            }else if(numbers.charAt(i) == 'o'){
+                sb.append("1");
+                i+=2;
+            }else if(numbers.charAt(i) == 't' && numbers.charAt(i+1) == 'w'){
+                sb.append("2");
+                i+=2;
+            }else if(numbers.charAt(i) == 't' && numbers.charAt(i+1) == 'h'){
+                sb.append("3");
+                i+=4;
+            }else if(numbers.charAt(i) == 'f' && numbers.charAt(i+1) == 'o'){
+                sb.append("4");
+                i+=3;
+            }else if(numbers.charAt(i) == 'f' && numbers.charAt(i+1) == 'i'){
+                sb.append("5");
+                i+=3;
+            }else if(numbers.charAt(i) == 's' && numbers.charAt(i+1) == 'i'){
+                sb.append("6");
+                i+=2;
+            }else if(numbers.charAt(i) == 's' && numbers.charAt(i+1) == 'e'){
+                sb.append("7");
+                i+=4;
+            }else if(numbers.charAt(i) == 'e'){
+                sb.append("8");
+                i+=4;
+            }else if(numbers.charAt(i) == 'n'){
+                sb.append("9");
+                i+=3;
+            }
+        }
+        return Long.parseLong(sb.toString());
+    }
+}
