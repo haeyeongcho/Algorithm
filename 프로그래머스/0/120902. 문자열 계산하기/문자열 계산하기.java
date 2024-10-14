f11bfd8d7ae3bc2a58066aa7bf0b6b3c19f9c0cb
Haeyeong Cho <98633822+haeyeongcho@users.noreply.github.com>
2024-10-15 05:52:55 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.14 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] cal = my_string.split(" ");
+        answer = Integer.parseInt(cal[0]);
+        for(int i = 1; i < cal.length; i+=2){
+            if(cal[i].equals("+")){
+                answer += Integer.parseInt(cal[i+1]);
+            }else{
+                answer -= Integer.parseInt(cal[i+1]);
+            }
+        }
+        return answer;
+    }
+}
